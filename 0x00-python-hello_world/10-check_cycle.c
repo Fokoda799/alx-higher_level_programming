605e6243d58f3805ad51f5b5aaecb1015a0aa441
Fokoda799 <feyt2003@gmail.com>
2023-11-28 22:30:00 +0300

    update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,12 +1,16 @@
 #include "lists.h"
 
-
+/**
+* check_cycle - check if linked list has a cycel.
+* @list: linked list header.
+* Return: 1 if it has a cycle, 0 if not.
+*/
 int check_cycle(listint_t *list)
 {
 	listint_t *tree = list;
 	listint_t *one = list;
 
-	while(tree != NULL && tree->next != NULL && tree->next->next != NULL)
+	while (tree != NULL && tree->next != NULL && tree->next->next != NULL)
 	{
 		tree = tree->next->next->next;
 		one = one->next;
