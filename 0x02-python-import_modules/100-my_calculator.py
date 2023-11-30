dbaa27f390bcb4da38cb5f04cd26a17431557d44
Fokoda799 <feyt2003@gmail.com>
2023-12-01 00:14:35 +0300

    task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,11 @@
 #!usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+    from calculator_1 import add, sub, mul, div
 
     _len = len(argv)
+    a = int(argv[1])
+    b = int(argv[3])
     op = ['+', '-', '*', '/']
     if _len != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
