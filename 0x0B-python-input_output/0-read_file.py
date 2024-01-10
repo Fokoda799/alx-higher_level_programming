d03966dd0c03b5c5b48f52f71a94e6d81125caf9
Fokoda799 <feyt2003@gmail.com>
2024-01-11 02:19:36 +0300

    all

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Read file"""
+
+
+def read_file(filename=""):
+    """Read a text file and
+    prints it to stdout """
+
+    with open(filename, 'r') as file:
+        text = file.read()
+        
+    print(text)
