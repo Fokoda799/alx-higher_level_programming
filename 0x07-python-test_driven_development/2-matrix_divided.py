f04ffaa743d6614fedf12bf14e80ea866c66305d
Fokoda799 <feyt2003@gmail.com>
2024-01-10 15:29:29 +0300

    fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,34 +1,42 @@
 #!/usr/bin/python3
-"""Divide a matrix
+"""Divide a matrix.
 
-This module has one function that divides all
-elements of a matrix.
+This module has one function that divides all elements of a matrix.
 """
 
 
 def matrix_divided(matrix, div):
-    """Matrix divided function
-       Args: matrix(list) and div(number)
-       Return: A new matrix"""
+    """Matrix divided function.
 
-    m="matrix must be a matrix (list of lists) of integers/floats"
-    n="Each row of the matrix must have the same size"
+    Args: matrix(list) and div(number)
+    Return: A new matrix.
+    """
 
-    if not all(isinstance(row, list) and \
-    all(isinstance(num, (int, float)) for num in row) \
-    for row in matrix):
-        raise TypeError(m)
+    error_messages = {
+        "type": "matrix must be a matrix (list of lists) of integers/floats",
+        "rows_size": "Each row of the matrix must have the same size",
+        "div_type": "div must be a number",
+        "div_zero": "division by zero",
+    }
 
-    i = 0
+    # Check matrix is a list of lists
     for row in matrix:
-        if len(matrix[i]) !=  len(matrix[i+1]):
-            raise TypeError(n)
+        for num in row:
+            if not isinstance(row, list) and not isinstance(num, (int, float)):
+                raise TypeError(error_messages["type"])
 
+    # Check rows have the same size
+    row_sizes = set(len(row) for row in matrix)
+    if len(row_sizes) != 1:
+        raise TypeError(error_messages["rows_size"])
+
+    # Check div is a number and not zero
     if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
+        raise TypeError(error_messages["div_type"])
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError(error_messages["div_zero"])
 
+    # Divide each element in the matrix by div
     return [[round(num / div, 2) for num in row] for row in matrix]
 
 
