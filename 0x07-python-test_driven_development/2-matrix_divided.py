364e1ade512914a7cc6687835671b1c49abd299c
Fokoda799 <feyt2003@gmail.com>
2024-01-10 15:47:18 +0300

    fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,45 +1,25 @@
-#!/usr/bin/python3
-"""Divide a matrix.
-
-This module has one function that divides all elements of a matrix.
-"""
-
+#!/bin/usr/python3
 
 def matrix_divided(matrix, div):
-    """Matrix divided function.
 
-    Args: matrix(list) and div(number)
-    Return: A new matrix.
-    """
+    m = 'matrix must be a matrix (list of lists) of integers/floats'
 
-    error_messages = {
-        "type": "matrix must be a matrix (list of lists) of integers/floats",
-        "rows_size": "Each row of the matrix must have the same size",
-        "div_type": "div must be a number",
-        "div_zero": "division by zero",
-    }
-
-    # Check matrix is a list of lists
-    for row in matrix:
-        for num in row:
-            if not isinstance(row, list) and not isinstance(num, (int, float)):
-                raise TypeError(error_messages["type"])
-
-    # Check rows have the same size
-    row_sizes = set(len(row) for row in matrix)
-    if len(row_sizes) != 1:
-        raise TypeError(error_messages["rows_size"])
-
-    # Check div is a number and not zero
     if not isinstance(div, (int, float)):
-        raise TypeError(error_messages["div_type"])
+        raise TypeError('div must be a number')
     if div == 0:
-        raise ZeroDivisionError(error_messages["div_zero"])
-
-    # Divide each element in the matrix by div
-    return [[round(num / div, 2) for num in row] for row in matrix]
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/2-matrix_divided.txt")
+        raise ZeroDivisionError('division by zero')
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(m)
+        if len(row) != len(matrix[0]):
+            raise TypeError('Each row of the matrix must have the same size')
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(m)
+    result = []
+    for row in matrix:
+        result_row = []
+        for element in row:
+            result_row.append(round(element / div, 2))
+        result.append(result_row)
+    return result
--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -2,9 +2,9 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    [1, 2, 3],
+    ['a', 2, 3],
     [4, 5, 6]
 ]
-print(matrix_divided(2))
-print(matrix)
+
+print(matrix_divided(matrix, 3))
 
