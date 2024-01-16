69ef57502885b5ca1033ef24a077096b402255ab
Fokoda799 <feyt2003@gmail.com>
2024-01-16 20:14:46 +0300

    $1

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,8 +6,4 @@ import json
 def class_to_json(obj):
     """Class to JSON"""
 
-    obj_dict = {}
-    for key, value in vars(obj).items():
-        if isinstance(value, (int, dict, str, list, bool)):
-            obj_dict[key] = value
-    return obj_dict
+    return obj.__dict__
