728e965b87d1eaf43dca28c15e3d3a84bb57d90a
Fokoda799 <feyt2003@gmail.com>
2024-01-17 05:06:47 +0300

    $1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,8 +8,12 @@ class Rectangle(Base):
 
     # Private instance attributes
     def __init__(self, width, height, x=0, y=0, id=None):
+        dict = {'width':width, 'height':height, 'x':x, 'y':y}
+        for attr in dict:
+            self.int_validation(attr, dict[attr])
+            self.pos_validation(attr, dict[attr])
         self.__width = width
-        self.__height = height
+        self.__height = height 
         self.__x = x
         self.__y = y
         super().__init__(id)
@@ -34,16 +38,36 @@ class Rectangle(Base):
     # Setters
     @width.setter
     def width(self, value):
+        self.int_validation('width', value)
+        self.pos_validation('width', value)
         self.__width = value
 
     @height.setter
     def height(self, value):
+        self.int_validation('height', value)
+        self.pos_validation('height', value)
         self.__height = value
 
     @x.setter
     def x(self, value):
+        self.int_validation('x', value)
+        self.pos_validation('x', value)
         self.__x = value
 
     @y.setter
     def y(self, value):
+        self.int_validation('y', value)
+        self.pos_validation('y', value)
         self.__y = value
+
+    def int_validation(self, attr_name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{attr_name} must be an integer")
+
+    def pos_validation(self, attr_name, value):
+        if attr_name in ['width', 'height']:
+            if value <= 0:
+                raise ValueError(f"{attr_name} must be > 0")
+        else:
+            if value < 0:
+                raise ValueError(f"{attr_name} must be >= 0")
